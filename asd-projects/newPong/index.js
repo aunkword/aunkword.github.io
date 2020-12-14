d51edc4901a38cdee0544a272a9ee1cfb6c610f9
aunkword <aunkword@gmail.com>
2020-12-14 20:25:44 +0000

    saving everything

--- asd-projects/newPong/index.js
+++ asd-projects/newPong/index.js
@@ -16,36 +16,36 @@ function runProgram(){
       s: 83,
   };
 
-  var boardWidth = $('#board').width();
-  var boardHeight = $('#board').height();
-  var paddleSpeed = 0;
-  var ballSpeedRate = 1.25;
-  var endScore = 7
+  var bwidth = $('#board').width();
+  var bheight = $('#board').height();
+  var score1 = 0;
+  var score2 = 0;
+  var endScore = 7;
 
   // Game Item Objects
   var ball = {
-    positionX: 190,
-    positionY: 190,
-    speedX: .5,
-    speedY: .5,
-    width: 20,
-    height: 20,
+    x: 190,
+    y: 190,
+    spX: .8,
+    spY: .8,
+    w: 10,
+    h: 10,
     id: '#ball',
   }
   var paddle1 = {
-    positionX: 30,
-    positionY: 150,
-    speedY: 0,
-    width: 10,
-    height: 85,
+    x: 30,
+    y: 150,
+    spY: 0,
+    w: 10,
+    h: 85,
     id: '#paddle1',
   }
   var paddle2 = {
-    positionX: 360,
-    positionY: 150,
-    speedY: 0,
-    width: 10,
-    height: 85,
+    x: 360,
+    y: 150,
+    spY: 0,
+    w: 10,
+    h: 85,
     id: '#paddle2',
   }
 
@@ -66,13 +66,16 @@ function runProgram(){
     updateBall();
     updatePaddles();
     doCollide();
+    boardCollide();
+    wallBounce();
+    score();
   }
   
   /* 
   Called in response to events.
   */
   function handleEvent(event) {
-    
+      
   }
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -80,71 +83,127 @@ function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
 
   function updateBall() {
-    ball.positionX += ball.speedX;
-    ball.positionY += ball.speedY
-    $(ball.id).css("left", ball.positionX);
-    $(ball.id).css("top", ball.positionY);
-  }
+    ball.x += ball.spX;
+    ball.y += ball.spY;
+    $(ball.id).css("left", ball.x);
+    $(ball.id).css("top", ball.y);
+}
 
   function handleKeyDown(event) {
     var keycode = event.which;
     if (keycode === key.w) {
-      paddle1.speedY -= 1;
+      paddle1.spY -= 1;
   } else if (keycode === key.s) {
-      paddle1.speedY += 1;
+      paddle1.spY += 1;
   } else if (keycode === key.up) {
-      paddle2.speedY -= 1;
+      paddle2.spY -= 1;
   } else if (keycode === key.down) {
-      paddle2.speedY += 1;
+      paddle2.spY += 1;
   }
 }
 
   function handleKeyUp() {
-    paddle1.speedY = 0;
-    paddle2.speedY = 0;  
-  }
+    paddle1.spY = 0;
+    paddle2.spY = 0;  
+}
 
   function updatePaddles() {
-    paddle1.positionY += paddle1.speedY;
-    paddle2.positionY += paddle2.speedY;
-    $(paddle1.id).css("top", paddle1.positionY);
-    $(paddle2.id).css("top", paddle2.positionY);
-    $(paddle1.id).css("left", paddle1.positionX);
-    $(paddle2.id).css("left", paddle2.positionX);
-  }
+    paddle1.y += paddle1.spY;
+    paddle2.y += paddle2.spY;
+    $(paddle1.id).css("top", paddle1.y);
+    $(paddle2.id).css("top", paddle2.y);
+    $(paddle1.id).css("left", paddle1.x);
+    $(paddle2.id).css("left", paddle2.x);
+}
 
-  function doCollide() {
-  
-    ball.leftX = ball.positionX;
-    ball.topY = ball.positionY;
-    ball.rightX = ball.positionX + ball.width;
-    ball.bottomY = ball.postionY + ball.height;
-  
-    paddle1.leftX = paddle1.positionX;
-    paddle1.topY = paddle1.positionY;
-    paddle1.rightX = paddle1.positionX + paddle1.width;
-    paddle1.bottomY = paddle1.postionY + paddle1.height;
+  function boardCollide() {
+    ball.left = ball.x;
+    ball.right = ball.x + ball.w;
+    ball.top = ball.y;
+    ball.bottom = ball.y + ball.h;
 
-    paddle2.leftX = paddle2.positionX;
-    paddle2.topY = paddle2.positionY;
-    paddle2.rightX = paddle2.positionX + paddle2.width;
-    paddle2.bottomY = paddle2.postionY + paddle2.height;
-	
-	if (ball.rightX < paddle1.leftX && ball.leftX < paddle1.rightX && ball.topY < paddle1.bottomY && ball.bottomY > paddle1.topY) {
-      console.log('yes');
-    } else if (ball.rightX > paddle2.leftX && ball.leftX > paddle2.rightX && ball.topY < paddle2.bottomY && ball.bottomY > paddle2.topY) {
-      console.log('yes');
-    } else {
-      console.log('no');
+    if (ball.left < 0 || ball.right > bwidth){
+        ball.x = 190;
+        ball.y = 190;
+    }
+    boardPaddleCollide();
+}
+
+  function boardPaddleCollide() {
+    paddle1.top = paddle1.y;
+    paddle1.bottom = paddle1.y + paddle1.h;
+    paddle2.top = paddle2.y;
+    paddle2.bottom = paddle2.y + paddle2.h;
+
+    if (paddle1.top <= 0) {
+        paddle1.y = 0;
+    } else if (paddle1.bottom >= bheight) {
+        paddle1.y = bheight - 85;
+    }
+    if (paddle2.top <= 0) {
+        paddle2.y = 0;
+    } else if (paddle2.bottom >= bheight) {
+        paddle2.y = bheight - 85;
     }
 }
 
-  function endGame() {
-    // stop the interval timer
-    clearInterval(interval);
-
-    // turn off event handlers
-    $(document).off();
-  }
+  function doCollide() {
   
+    ball.left = ball.x;
+    ball.right = ball.x + ball.w;
+    ball.top = ball.y;
+    ball.bottom = ball.y + ball.h;
+  
+    paddle1.left = paddle1.x;
+    paddle1.right = paddle1.x + paddle1.w;
+    paddle1.top = paddle1.y;
+    paddle1.bottom = paddle1.y + paddle1.h;
+
+    paddle2.left = paddle2.x;
+    paddle2.right = paddle2.x + paddle2.w;
+    paddle2.top = paddle2.y;
+    paddle2.bottom = paddle2.y + paddle2.h;
+    
+	if (ball.left <= paddle1.right && ball.right >= paddle1.left && ball.top <= paddle1.bottom && ball.bottom >= paddle1.top) {
+        bounceBack();
+    }
+    if (ball.right >= paddle2.left && ball.left <= paddle2.right && ball.top <= paddle2.bottom && ball.bottom >= paddle2.top) {
+        bounceBack();
+    }
+}
+
+  function bounceBack() {
+    ball.spX -= ball.spX * 2 * 1.05;
+}
+
+  function wallBounce() {
+    if (ball.top <= 0 || ball.bottom >= bheight) {
+        ball.spY -= ball.spY * 2;
+    }
+  }
+
+  function score () {
+    if (ball.left <= 0) {
+        score1 += 1;
+        $('#score1').text(score1);
+	} else if (ball.right >= bwidth) {
+        score2 += 1;
+        $('#score2').text(score2);
+    }
+
+    if (score1 === endScore) {
+        endGame();
+
+    } else if (score2 === endScore) {
+        endGame();
+    }
+}
+
+    function endGame() {
+        // stop the interval timer
+        clearInterval(interval);
+
+        // turn off event handlers
+        $(document).off();
+    }
 }
--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -9,7 +9,7 @@ function runProgram(){
 
   // Constant Variables
   
-  var fpsInterval = 100 / 60;
+  var fpsInterval = 100;
   var key = {
       'left': 37,
       'up': 38,
@@ -19,21 +19,21 @@ function runProgram(){
   var score = 0;
   var bwidth = $('#board').width();
   var bheight = $('#board').height();
-  var square = $("#apple").width();
+  var square = 20;
   
   // Game Item Objects
   
   var apple = {
-    x: 320,
-    y: 210,
+    x: 0,
+    y: 0,
     width: 20,
     height: 20,
     id: '#apple'
   }
 
   var head = {
-    x: 100,
-    y: 210,
+    x: randomInteger(bwidth/square) * square,
+    y: randomInteger(bheight/square) * square,
     speedX: 0,
     speedY: 0,
     width: 20,
@@ -52,24 +52,25 @@ function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
 
   function newFrame() {
-    borders();
     repoHead();
+    appleCollide();
+    boardCollide();
   }
 
   function keyDown(event) {
-    if (event.which === key.left) {
-        head.speedX = -1;
-        head.speedY = 0;
-  } else if (event.which === key.up) {
-        head.speedX = 0;
-        head.speedY = -1;
-  } else if (event.which === key.right) {
-        head.speedX = 1;
-        head.speedY = 0;
-  } else if (event.which === key.down) {
-        head.speedX = 0;
-        head.speedY = 1;
-  }
+        if (event.which === key.left) {
+            head.speedX = -20;
+            head.speedY = 0;
+    } else if (event.which === key.up) {
+            head.speedX = 0;
+            head.speedY = -20;
+    } else if (event.which === key.right) {
+            head.speedX = 20;
+            head.speedY = 0;
+    } else if (event.which === key.down) {
+            head.speedX = 0;
+            head.speedY = 20;
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -77,34 +78,86 @@ function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
 
   function repoHead() {
-      $("#head").css("left", head.x);
-      $("#head").css("top", head.y);
-      head.x += head.speedX;
-      head.y += head.speedY;
+    $("#head").css("left", head.x);
+    $("#head").css("top", head.y);
+    head.x += head.speedX;
+    head.y += head.speedY;
   }
 
-  function borders() {
+  function moveApple(){
+    apple.x = randomInteger(bwidth/square) * square;
+    apple.y = randomInteger(bheight/square) * square;
+    $('#apple').css('left', apple.x)
+    $('#apple').css('top', apple.y)
+      for (var i = 0; i < snakeBody.length; i++){
+        if(doCollide(apple,snakeBody[i])){
+            moveApple();
+        }
+      }
+    }
+
+  function body(id) {
+    var body = {};
+    body.id = id
+    body.x = 100;
+    body.y = 100;
+    body.width = $('.body').width();
+    body.height = $('.body').width();
+    return body;
+  }
+
+  function addNewBody(){
+    var newID = 'body' + snakeBody.length;
+    $('<div>').addClass('snake').attr('id', newID).appendTo('#board');
+    var newBody = body('#' + newID);
+  }
+
+  function boardCollide() {
     head.left = head.x;
     head.right = head.x + 20;
     head.top = head.y;
     head.bottom = head.y + 20;
 
-    if (head.left === 0 || head.right === bwidth || head.top === 0 || head.bottom === bheight){
+    if (head.left < 0 || head.right > bwidth || head.top < 0 || head.bottom > bheight){
         endGame();
         var again = prompt('Play again?');
-        if(again = 'yes'){
-           runProgram();
-        }
-    } else {}
+        if (again === 'yes'){
+        runProgram();
+      }
+    }
   }
 
-  function buildBody() {
-      var body = {};
-      // give it a class using jQuery (you will need to make CSS for the class in the CSS file)
-      // give it a unique id (technically optional, but it's helpful)
-      // return the object
-      // give it an x and y position
+  function appleCollide() {
+    head.left = head.x;
+    head.right = head.x + 20;
+    head.top = head.y;
+    head.bottom = head.y + 20;
+
+    apple.left = apple.x;
+    apple.right = apple.x + 20;
+    apple.top = apple.y;
+    apple.bottom = apple.y + 20;
+
+    if (head.left < apple.right && head.right > apple.left && head.top < apple.bottom && head.bottom > apple.top){
+        score++;
+        moveApple();
+    }
   }
+
+  function doCollide(obj1, obj2) {
+    if (obj1.x === obj2.x && obj1.y === obj2.y) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+  function randomInteger(max) {
+    var randomInt = Math.floor(Math.random() * max);
+    return randomInt;
+}
+
 
   function endGame() {
     // stop the interval timer
@@ -115,3 +168,5 @@ function runProgram(){
   }
   
 }
+
+// https://jsbin.com/fegigoderu/edit?js,output
